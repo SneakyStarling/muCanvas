cc16cb2546db49f263dd86c5d303087679bf702a
SneakyStarling <nope@example.com>
2025-04-18 14:40:00 +0200

    use arm64 runner

--- main.c
+++ main.c
@@ -6,12 +6,19 @@ int main() {
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
 
-    SDL_Window *window = SDL_CreateWindow("SDL2 Text", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2 Text",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        SDL_WINDOW_FULLSCREEN
+    );
+//    SDL_Window *window = SDL_CreateWindow("SDL2 Text", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     TTF_Font *font = TTF_OpenFont("DejaVuSans.ttf", 24);
     SDL_Color color = {255, 255, 255, 255};
-    SDL_Surface *text_surface = TTF_RenderText_Solid(font, "Hello SDL2!", color);
+    SDL_Surface *text_surface = TTF_RenderText_Solid(font, "Hello world!", color);
     SDL_Texture *text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
 
     SDL_Rect dstrect = {50, 50, text_surface->w, text_surface->h};
@@ -21,7 +28,7 @@ int main() {
     SDL_RenderCopy(renderer, text_texture, NULL, &dstrect);
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(2000);
+    SDL_Delay(20000);
 
     SDL_DestroyTexture(text_texture);
     TTF_CloseFont(font);
